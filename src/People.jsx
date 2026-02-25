494d51eefb72fcb44bacda5bdecf6b06fe8134ec
mdiffatbari <iffatbaribappy1999@gmail.com>
2026-02-25 20:54:41 +0600

    clearing project

--- src/People.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-export default function People({people}){
-  return(
-    <div className="border22">
-        <h1>People: {people}</h1>
-    </div>
-  )
-}
--- src/Todo.jsx
+++
@@ -1,6 +0,0 @@
-export default function Todo({ task, isDone }){
-    if (isDone === true) {
-        return <li className="border22">task: {task}</li>
-    }
-    return <li>To be done: {task} </li>
-}
